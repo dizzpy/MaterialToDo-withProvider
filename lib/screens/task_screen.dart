85e2b2b5f16e114260fb3a1931e9f03280c28a25
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-06-25 01:38:12 +0530

    ui done

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -5,6 +5,57 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Top Section
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Task List',
+                      style: TextStyle(
+                        fontSize: 26,
+                      ),
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      '12 Tasks',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // List View
+              Expanded(
+                child: Container(
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        title: Text('This is task 1'),
+                        trailing: Checkbox(
+                          value: false,
+                          onChanged: (bool) {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
