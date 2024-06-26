a208d9d658d520be86b5cc29f7f26349939d7449
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-06-26 20:30:35 +0530

    checkbox done

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -10,11 +10,29 @@ class TaskTitle extends StatelessWidget {
     return Card(
       child: ListTile(
         title: Text('This is task 1'),
-        trailing: Checkbox(
-          value: true,
-          onChanged: (bool) {},
-        ),
+        trailing: TaskCheckBox(),
       ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  State<TaskCheckBox> createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (newvalue) {
+        setState(() {
+          isChecked = newvalue!;
+        });
+      },
     );
   }
 }
